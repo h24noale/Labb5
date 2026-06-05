7922aa13bc2d249dff8d3ba17b497f5949c5e62a
Camilla Tillander <camilla.tillander@outlook.com>
2026-06-05 21:21:38 +0200

    Lagt till algortimer

--- AlgorithmLib/SortingManager.cs
+++ AlgorithmLib/SortingManager.cs
@@ -103,16 +103,71 @@ namespace AlgorithmLib
                 k++;
             }
         }
-        /// <summary>
-        /// Sorterar listan med Heap Sort-algoritmen.
-        /// </summary>
-        /// <param name="collection">Listan som ska sorteras.</param>
         public void HeapSort(IList<T> collection)
         {
-            throw new NotImplementedException();
+            int n = collection.Count;
+            // Bygg max-heap 
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                int root = i;
+                while (true)
+                {
+                    int left = 2 * root + 1;
+                    int right = 2 * root + 2;
+                    int largest = root;
+                    if (left < n &&
+                    ((IComparable<T>)collection[left]).CompareTo(collection[largest]) > 0)
+                    {
+                        largest = left;
+                    }
+                    if (right < n &&
+                    ((IComparable<T>)collection[right]).CompareTo(collection[largest]) > 0)
+                    {
+                        largest = right;
+                    }
+                    if (largest == root)
+                        break;
+                    T temp = collection[root];
+                    collection[root] = collection[largest];
+                    collection[largest] = temp;
+                    root = largest;
+                }
+            }
+            // Sortera genom att plocka från heapen 
+            for (int i = n - 1; i > 0; i--)
+            {
+                T temp = collection[0];
+                collection[0] = collection[i];
+                collection[i] = temp;
+                int root = 0;
+                int heapSize = i;
+                while (true)
+                {
+                    int left = 2 * root + 1;
+                    int right = 2 * root + 2;
+                    int largest = root;
+                    if (left < heapSize &&
+                    ((IComparable<T>)collection[left]).CompareTo(collection[largest]) > 0)
+                    {
+                        largest = left;
+                    }
+                    if (right < heapSize &&
+                    ((IComparable<T>)collection[right]).CompareTo(collection[largest]) > 0)
+                    {
+                        largest = right;
+                    }
+                    if (largest == root)
+                        break;
+                    temp = collection[root];
+                    collection[root] = collection[largest];
+                    collection[largest] = temp;
+                    root = largest;
+                }
+            }
         }
 
-        
+
+
         /// <summary>
         /// Sorterar listan med Quick Sort-algoritmen.
         /// </summary>
